23bf40101e8e0e8b9a3ec34d63d758d29b3a77ea
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-04-15 03:53:25 +0300

    3 задание
    обьект с полем message для локального хранилища

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle'
 
+const STORAGE_REY = 'feedback-form-state';
+
+
+const refs ={
+    form:document.querySelector('.feedback-form'),
+    textarea:document.querySelector('textarea'),
+    //input:document.querySelector('input'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTexteriaInput, 500));
+
+
+textareaPop();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+
+ //метод который очищает форму
+ evt.currentTarget.reset();
+
+ localStorage.removeItem('STORAGE_REY');
+}
+
+function onTexteriaInput(evt) {
+     const message = evt.target.value;
+
+     console.log(message)
+     localStorage.setItem('STORAGE_REY', message);
+}
+
+function textareaPop() {
+  const saveMessage = localStorage.getItem('STORAGE_REY');
+
+    if(saveMessage){
+         console.log(saveMessage);
+         refs.textarea.value = saveMessage;
+     }
+   
+}
