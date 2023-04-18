54331cb451049466a36c683935624f8e4e5972eb
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-04-18 20:36:16 +0000

    Deploying to gh-pages from @ Iryna-kondreskul/goit-js-hw-08@a72038d55a929ec37526644ac082514ba2aacd5d 🚀

---
+++ 02-video.dd6c6f11.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe");console.log(e),player.on("play",(function(){console.log("played the video!")})),player.getVideoTitle().then((function(e){console.log("title:",e)}))}();
+//# sourceMappingURL=02-video.dd6c6f11.js.map
