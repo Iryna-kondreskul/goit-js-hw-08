377d7761d409df0c5b624a58ffddabdda7621519
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-04-24 03:06:50 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,12 @@
 import throttle from 'lodash.throttle';
-//import playar from '@vimeo/player';
 
-const iframeEl = document.querySelector('iframe');
-console.log(iframeEl);
-
-const playerEl = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds);
-  }
-  
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
